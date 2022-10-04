bfc372c90aaaabbc3c8e2cb590c13461ab86f8a0
01-ERFA <atukdibe@gmail.com>
2022-10-04 20:00:57 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	let digits = [props.digit5, props.digit4, props.digit3, props.digit2, props.digit1, props.digit0]
+
+	const digits_screen = digits.map((item)=>
+	<div className="text-light ms-2 ps-2 pe-2" style={{fontSize: "50px", background: "#182C61", borderRadius:"10px"}}>{item}</div>)
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<main className="bg-dark p-5">
+            <div className="container-fluid d-flex justify-content-center">
+                {/* <p className="text-light ms-3">0</p> */}
+				<div className="text-light me-1 ps-2 pe-2" style={{fontSize: "50px", background: "#182C61", borderRadius:"10px"}}><i className="far fa-clock"></i></div>
+				{digits_screen}
+            </div>
+        </main>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,53 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let digit0 = 0
+let digit1 = 0
+let digit2 = 0
+let digit3 = 0
+let digit4 = 0
+let digit5 = 0
+let wall = 10
+
+setInterval(() => {ReactDOM.render( < Home digit0 = {digit0} digit1 = {digit1} digit2 = {digit2} digit3 = {digit3} digit4 = {digit4} digit5 = {digit5}/>, 
+                document.querySelector("#app"));
+                    digit0++;
+                    if(digit0 === wall){
+                        digit0 = 0
+                        digit1++
+                    }
+                    if(digit1 === wall) {
+                        digit0 = 0
+                        digit1 = 0
+                        digit2++
+                    }
+                    if(digit2 === wall) {
+                        digit0 = 0
+                        digit1 = 0
+                        digit2 = 0
+                        digit3++
+                    }
+                    if(digit3 === wall) {
+                        digit0 = 0
+                        digit1 = 0
+                        digit2 = 0
+                        digit3 = 0
+                        digit4++
+                    }
+                    if(digit4 === wall) {
+                        digit0 = 0
+                        digit1 = 0
+                        digit2 = 0
+                        digit3 = 0
+                        digit4 = 0
+                        digit5++
+                    }
+                    if(digit5 === wall) {
+                        digit0 = 0
+                        digit1 = 0
+                        digit2 = 0
+                        digit3 = 0
+                        digit4 = 0
+                        digit5 = 0
+                    }
+            }, 1000) 
